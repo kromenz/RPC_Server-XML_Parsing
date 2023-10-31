1a077e295279a6c3cc3589b606ffbb68effd093a
kromenz <rafael.pires.andre@ipvc.pt>
2023-10-31 14:30:48 +0000

    ficheiro xml formatado
    ficheiro xml formatado da forma correta, com a hierarquia bem definida

--- systems-integration-tp1/src/samples/xml-generation/csv_to_xml_converter.py
+++ systems-integration-tp1/src/samples/xml-generation/csv_to_xml_converter.py
@@ -7,63 +7,56 @@ class CSVtoXMLConverter:
         self._reader = CSVReader(csv_path)
         self.xml_path = csv_path.replace('.csv', '.xml')
         self.people_ids = {}
-        self.country_ids = {}
         self.car_ids = {}
+        self.country_ids = {}
+        self.payment_ids = {}
+        self.car_model_ids = {}
 
-    def get_person_id(self, first_name, last_name):
-        full_name = f"{first_name} {last_name}"
-        if full_name in self.people_ids:
-            return self.people_ids[full_name]
-        else:
-            person_id = len(self.people_ids) + 1
-            self.people_ids[full_name] = person_id
-            return person_id
-
-    def get_country_id(self, country):
-        if country in self.country_ids:
-            return self.country_ids[country]
-        else:
-            country_id = len(self.country_ids) + 1
-            self.country_ids[country] = country_id
-            return country_id
-
-    def get_car_id(self, brand, model):
-        car_identifier = f"{brand} {model}"
-        if car_identifier in self.car_ids:
-            return self.car_ids[car_identifier]
-        else:
-            car_id = len(self.car_ids) + 1
-            self.car_ids[car_identifier] = car_id
-            return car_id
+    def get_unique_id(self, id_dict, value):
+        if value not in id_dict:
+            id_dict[value] = len(id_dict) + 1
+        return id_dict[value]
 
     def to_xml(self):
         root_el = ET.Element("Data")
 
         for row in self._reader.loop():
             # Create person element
-            person_id = self.get_person_id(row["First Name"], row["Last Name"])
+            person_id = self.get_unique_id(self.people_ids, f"{row['First Name']} {row['Last Name']}")
             person_el = ET.Element("Person", id=str(person_id))
             person_el.set("FirstName", row["First Name"])
             person_el.set("LastName", row["Last Name"])
 
+            # Create car element
+            car_id = self.get_unique_id(self.car_ids, row["Car Brand"])
+            car_el = ET.Element("Car", id=str(car_id))
+            car_el.set("Brand", row["Car Brand"])
+
+            # Create car model element
+            car_model_id = self.get_unique_id(self.car_model_ids, f"{row['Car Brand']} {row['Car Model']}")
+            car_model_el = ET.Element("Model", id=str(car_model_id))
+            car_model_el.set("Name", row["Car Model"])
+            car_model_el.set("Color", row["Car Color"])
+            car_model_el.set("Year", row["Year of Manufacture"])
+
+            car_el.append(car_model_el)
+
             # Create country element
-            country_id = self.get_country_id(row["Country"])
+            country_id = self.get_unique_id(self.country_ids, row["Country"])
             country_el = ET.Element("Country", id=str(country_id))
             country_el.text = row["Country"]
 
-            # Create car element
-            car_id = self.get_car_id(row["Car Brand"], row["Car Model"])
-            car_el = ET.Element("Car", id=str(car_id))
-            car_el.set("Brand", row["Car Brand"])
-            car_el.set("Model", row["Car Model"])
-            car_el.set("Color", row["Car Color"])
-            car_el.set("Year", row["Year of Manufacture"])
-            car_el.set("CreditCardType", row["Credit Card Type"])
+            # Create payment element
+            payment_id = self.get_unique_id(self.payment_ids, row["Credit Card Type"])
+            payment_el = ET.Element("Payment", id=str(payment_id))
+            payment_el.text = row["Credit Card Type"]
 
-            # Add elements to the root
+            # Organize the hierarchy
+            person_el.append(country_el)
+            person_el.append(car_el)
+            person_el.append(payment_el)
+
             root_el.append(person_el)
-            root_el.append(country_el)
-            root_el.append(car_el)
 
         return ET.ElementTree(root_el)
 
@@ -74,9 +67,9 @@ class CSVtoXMLConverter:
 # Caminho para o arquivo CSV
 csv_path = 'docker/volumes/data/cars.csv'
 
-# Exemplo de como usar a classe CSVtoXMLConverter para converter o CSV para XML com IDs baseados em valores únicos
+# Exemplo de como usar a classe CSVtoXMLConverter para converter o CSV para XML com hierarquias e IDs únicos organizados
 converter = CSVtoXMLConverter(csv_path)
 converter.save_xml()
 
-# O arquivo XML hierárquico com IDs baseados em valores únicos será criado na mesma pasta que o arquivo CSV com o mesmo nome, mas com extensão .xml
+# O arquivo XML com hierarquias e IDs únicos será criado na mesma pasta que o arquivo CSV com o mesmo nome, mas com extensão .xml
 print(f'Arquivo XML criado em: {converter.xml_path}')
