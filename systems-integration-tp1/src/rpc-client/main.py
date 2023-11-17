a9e1e9f042b4b8b51aa0c762b289ce2b2e38820e
Oigres2 <sergiovb2002@hotmail.com>
2023-11-17 13:48:41 +0000

    Added documents and Brands

--- systems-integration-tp1/src/rpc-client/main.py
+++ systems-integration-tp1/src/rpc-client/main.py
@@ -1,4 +1,42 @@
 import xmlrpc.client 
 
+def list_documents():
+    documents = server.list_documents()
+    for doc in documents:
+        print(doc[0])  # Supondo que 'doc' seja uma tupla com o nome do arquivo
+
+def delete_document():
+    file_name = input("Digite o nome do arquivo para excluir: ")
+    if server.delete_document(file_name):
+        print("Documento excluído com sucesso.")
+        
+def list_brands():
+    try:
+        brands = server.fetch_brands()
+        if brands:
+            print("List of Brands:")
+            for brand in brands:
+                print(f"- {brand}")
+        else:
+            print("No brands found.")
+    except Exception as e:
+        print(f"Error: {e}")
+
 print("connecting to server...")
 server = xmlrpc.client.ServerProxy('http://is-rpc-server:9000')
+
+while True:
+    print("1. Listar documentos")
+    print("2. Deletar documento")
+    print("3. Listar Brands")
+    print("0. Sair")
+    choice = input("Escolha uma opção: ")
+
+    if choice == '1':
+        list_documents()
+    elif choice == '2':
+        delete_document()
+    elif choice == '3':
+        list_brands()
+    elif choice == '0':
+        break
--- systems-integration-tp1/src/rpc-server/main.py
+++ systems-integration-tp1/src/rpc-server/main.py
@@ -3,13 +3,46 @@ from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 from functions.csv_to_xml import CSVtoXMLConverter
 from models.database import Database
+from lxml import etree
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
+    
+def list_documents():
+    db = Database()
+    documents = db.selectAll("SELECT file_name FROM public.documents")
+    db.disconnect()
+    return documents
+
+def delete_document(file_name):
+    db = Database()
+    db.delete(f"DELETE FROM public.documents WHERE file_name = %s", (file_name,))
+    db.disconnect()
+    return True
+
+def fetch_brands():
+    database = Database()
+    query = "SELECT xml FROM public.documents WHERE file_name = %s"
+    data = ('/data/cars.xml',)  # Substitua pelo nome correto do arquivo, se necessário
+    result = database.select_one(query, data)
+    database.disconnect()
+
+    if result is not None:
+        xml_data = result[0]
+        root = etree.fromstring(xml_data)
+        brands = root.xpath('//Brand')
+        return [brand.get('name') for brand in brands]
+    else:
+        return []
 
 
 with SimpleXMLRPCServer(('0.0.0.0', 9000), requestHandler=RequestHandler) as server:
     server.register_introspection_functions()
+    
+    server.register_function(list_documents, "list_documents")
+    server.register_function(delete_document, "delete_document")
+    server.register_function(fetch_brands)
+
 
     db = Database()
 
